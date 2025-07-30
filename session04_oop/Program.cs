021eddac811640af5ce3766d073278fc590701c9
Yousef754 <yosefesam987@gmail.com>
2025-07-30 21:57:00 +0300

    Q1

--- session04_oop/Program.cs
+++ session04_oop/Program.cs
@@ -4,7 +4,70 @@
     {
         static void Main(string[] args)
         {
+            #region Q1-1
+            
+            /*1. Define Class Duration To include Three Attributes Hours,
+              Minutes and Seconds.
+             
 
+            2. Define All Required Constructors to Produce this output:
+                Duration D1 =new Duration (1,10,15);
+                D1.ToString();
+                Output: Hours: 1, Minutes :10, Seconds :15
+                Duration D1 =new Duration (3600);
+                D1.ToString();
+                Output: Hours: 1, Minutes :0, Seconds :0
+                Duration D2 =new Duration (7800);
+                D2.ToString();
+                Output: Hours: 2, Minutes :10, Seconds :0
+                Duration D3 =new Duration (666);
+                D3.ToString();
+                Output: Minutes :11, Seconds :6
+            */
+
+            //Duration D1 = new Duration(1, 10, 15);
+            //Console.WriteLine(D1);
+
+            // D1 = new Duration(3600);
+            //Console.WriteLine(D1);
+
+
+            //Duration D2 = new Duration(7800);
+            //Console.WriteLine(D2);
+
+            //Duration D3 = new Duration(666);
+            //Console.WriteLine(D3);
+
+
+            /*
+            3. Implement All required Operators overloading to enable this
+               Code:
+                D3=D1+D2
+                D3=D1 + 7800
+                D3=666+D3
+                D3= ++D1 (Increase One Minute)
+                D3 = --D2 (Decrease One Minute)
+                D1= D1 -D2
+                If (D1>D2)
+
+            */
+            //0-D3=D1 + D2;
+            //Console.WriteLine(D3);
+            // D3=D1 + 7800 xxxxxxxxxxxx
+            //D3 = 666 + D3  xxxxxxxxxxxx
+            //D3 = D1 + D3;
+            //Console.WriteLine(D3);
+            //D3 = ++D1;
+            //D3 = --D2
+            //D1= D1 - D2
+            //if(D1 > D2)
+            //{
+            //    Console.WriteLine("time in D1 bigger than D2");
+            //}
+
+
+            
+            #endregion
 
 
 
